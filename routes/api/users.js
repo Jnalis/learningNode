5bc4482bd58d27696eec65bc84cd5e2740cb0baa
Juvenalis Swai <swaijuvenalis@gmail.com>
2019-09-05 09:05:44 +0300

    'commit_user_registration'

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } require('express-validator/check');
+const {
+    check,
+    validationResult
+} = require('express-validator/check');
 
 //@route   POST api/Users
 //@route   Register Users
 //@rout    access Public
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('User route');
-});
+router.post('/', [
+        check('name', 'Name is Require')
+        .not()
+        .isEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Please enter a valid password with 6 or more characters').isLength({
+            min: 6
+        })
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            })
+        }
+
+        res.send('User route');
+    });
 
 module.exports = router;
