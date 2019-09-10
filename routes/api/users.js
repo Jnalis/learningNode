379b510284205baf930732ab48f0fc6d3789ed5b
Juvenalis Swai <swaijuvenalis@gmail.com>
2019-09-10 09:01:38 +0300

    'commit_profiles'

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,98 +12,97 @@ const User = require('../../models/User');
 //@route   POST api/Users
 //@route   Register Users
 //@rout    access Public
-router.post('/', [
-        check('name', 'Name is Require')
-        .not()
-        .isEmpty(),
-        check('email', 'Please include a valid email').isEmail(),
-        check('password', 'Please enter a valid password with 6 or more characters')
-        .isLength({
-            min: 6
-        })
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
+router.post(
+  '/',
+  [
+    check('name', 'Name is Require')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a valid password with 6 or more characters'
+    ).isLength({
+      min: 6
+    })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array()
-            })
-        }
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
 
+    const { name, email, password } = req.body;
 
-        const {
-            name,
-            email,
-            password
-        } = req.body;
+    try {
+      //See if user exist
+      let user = await User.findOne({
+        email
+      });
 
-        try {
-            //See if user exist
-            let user = await User.findOne({
-                email
-            });
-
-            if (user) {
-                return res.status(400).json({
-                    errors: [{
-                        msg: 'User already exists'
-                    }]
-                });
+      if (user) {
+        return res.status(400).json({
+          errors: [
+            {
+              msg: 'User already exists'
             }
+          ]
+        });
+      }
 
-            //Get users gravatar
+      //Get users gravatar
 
-            const avatar = gravatar.url(email, {
-                s: '200',
-                r: 'pg',
-                d: 'mm'
-            })
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
 
-            user = new User({
-                name,
-                email,
-                avatar,
-                password
-            });
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
 
-            //Encrypt password
-            const salt = await bcrypt.genSalt(10);
+      //Encrypt password
+      const salt = await bcrypt.genSalt(10);
 
-            user.password = await bcrypt.hash(password, salt);
+      user.password = await bcrypt.hash(password, salt);
 
-            //save to db
-            await user.save();
+      //save to db
+      await user.save();
 
-            //Return jsonwebtoken
+      //Return jsonwebtoken
 
-            const payload = {
-                user: {
-                    id: user.id
-                }
-            }
-
-            jwt.sign(
-                payload,
-                config.get('jwtSecret'), {
-                    expiresIn: 360000
-                },
-                (err, token) => {
-                    if (err) throw err;
-                    res.json({
-                        token
-                    });
-                }
-            );
-            //res.send('User registered');
-
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server Error');
-
+      const payload = {
+        user: {
+          id: user.id
         }
+      };
 
-
-    });
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            token
+          });
+        }
+      );
+      //res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
